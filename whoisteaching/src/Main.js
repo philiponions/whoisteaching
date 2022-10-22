fb87e7ae7b7b752cbc00e3d1063be34429cdb516
philiponions <hueycoy9@gmail.com>
2022-10-22 16:58:29 -0600

    changed style

--- whoisteaching/src/Main.js
+++ whoisteaching/src/Main.js
@@ -13,6 +13,7 @@ import { Container } from '@mui/system';
 import ErrorSnackbar from './Components/ErrorSnackbar';
 import NotFoundError from './Components/NotFoundError';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import {styled} from '@mui/material/styles';
 
 function Main() {
   
@@ -20,7 +21,7 @@ function Main() {
     searchField: {
       paddingTop: "10px",
       paddingBottom: "10px", 
-      justifyContent: "flex",
+      display: "flex",
       alignItems: "center",
       backgroundColor: "white",
       maxWidth: "100%",
@@ -62,6 +63,32 @@ function Main() {
       color: "white"
     }
   }
+
+  const Responsive = styled("div")(({ theme }) => ({
+    [theme.breakpoints.up("md")]: {
+      display: "flex",      
+    },
+    paddingTop: "10px",
+      paddingBottom: "10px", 
+      display: "block",
+      alignItems: "center",
+      backgroundColor: "white",
+      maxWidth: "100%",
+      justifyContent: "center",
+  }));
+
+  const TextField1 = styled("div")(({ theme }) => ({
+    [theme.breakpoints.up("md")]: {
+      display: "flex",      
+    },
+    paddingTop: "10px",
+      paddingBottom: "10px", 
+      display: "block",
+      alignItems: "center",
+      backgroundColor: "white",
+      maxWidth: "100%",
+      justifyContent: "center",
+  }));
 
   const [courseInput, setCourseInput] = useState("")
   const [numberInput, setNumberInput] = useState("")
@@ -121,14 +148,15 @@ function Main() {
       <header style={styles.header}>
         <div style={styles.bar}>
             <Typography variant="h2" style={styles.text}>Who is Teaching?</Typography>
-            <p style={styles.text}>View the current professors who are teaching this year at the University of Alberta.</p>
-            <div style={styles.searchField}>          
-              <TextField style={{marginRight: "10px"}} label="Course name" placeholder='Ex) AN SC'  onChange={(e) => setCourseInput(e.target.value)} onKeyDown={(e) => pressedEnter(e)}/>
-              <TextField label="Catalogue no." placeholder='Ex) 101' onChange={(e) => setNumberInput(e.target.value)} onKeyDown={(e) => pressedEnter(e)}/>
-              <IconButton onClick={getData}>
-                  <SearchIcon />
-              </IconButton>          
+            <p style={styles.text}>View the current professors who are teaching this year at the University of Alberta.</p>                        
+            <div style={styles.searchField}>
+              <div><TextField label="Course name" placeholder='Ex) AN SC'  onChange={(e) => setCourseInput(e.target.value)} onKeyDown={(e) => pressedEnter(e)}/></div>
+              <div><TextField label="Catalogue no." placeholder='Ex) 101' onChange={(e) => setNumberInput(e.target.value)} onKeyDown={(e) => pressedEnter(e)}/></div>
+              <IconButton onClick={getData}><SearchIcon /></IconButton>          
+            
+
             </div>
+            {/* </Responsive> */}
             <Typography style={styles.sampleTextField}>Ex) Course name: AN SC, Catalogue num: 101</Typography>
         </div>
           {!loaded ? <></> : <CircularIndeterminate/>}   
